94c682c05e3c967d95453c313965e44d32fc0463
Kuch72 <72315648+Kuch72@users.noreply.github.com>
2020-10-03 20:23:02 +0200

    Add files via upload

---
+++ DDoS_API.py
@@ -0,0 +1,64 @@
+print("     __              __       ____  ____       _____    ______            __")
+print("    / /____  _______/ /_     / __ \/ __ \____ / ___/   /_  __/___  ____  / /")
+print("   / //_/ / / / ___/ __ \   / / / / / / / __ \\__ \     / / / __ \/ __ \/ /") 
+print("  / ,< / /_/ / /__/ / / /  / /_/ / /_/ / /_/ /__/ /    / / / /_/ / /_/ / /")  
+print(" /_/|_|\__,_/\___/_/ /_/  /_____/_____/\____/____/    /_/  \____/\____/_/")   
+                                                                           
+print("")
+print("")                                       
+print("Welcome to Kuch#0001 API DDoS tool!")
+print("")
+
+import os, time, requests, webbrowser
+# Funciones 
+def borrarpant():
+    if os.name == "nt":
+        os.system ("cls")
+    else:
+        os.system ("clear")
+
+def kuch():
+    print(""" 1. For attack
+ 2. For exiting 
+ 3. For looking for my github""")
+
+## Fin funciones
+
+#Bucle 
+while True:
+    print("Select one option: ")
+    kuch()
+    atacar = input("Your option: ")
+    if atacar == "1":
+        borrarpant()
+        print("Kuch Attack hub")
+        time.sleep(1)
+        ip = input("Write target's IP: ")
+        port = input("Write target's port: ")
+        method = input("Write attack method: ")
+        time = input("Write attack time: ")
+        #conc = input("Write how much concurrents: ")
+        #r = requests.get("https://elpais.com/") ## Here put your api
+        print("Sending attacks...")
+        print("Thanks for using Kuch API DDoS tool. Exiting in 10 seconds...")
+        import time
+        time.sleep(10)
+        quit()
+    elif atacar == "2":
+        quit()
+    elif atacar == "3":
+        webbrowser.open("https://github.com/Kuch72")
+        print("There you have my github, u can also contact me via discord!: Kuch!#0001")
+        discord = input("Want to go to discord? Y/N: ")
+        if discord == "Y":
+            webbrowser.open("https://discord.com/")
+        elif discord == "N":
+            print("Okay, exiting...")
+            time.sleep(2)
+            quit()
+        else:
+            print("Invalid argument.")
+    else:
+        print("Invalid argument. ")
+
+        
